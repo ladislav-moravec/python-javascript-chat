e194c4b3cee4781986b3e7243e69d075ddd0dbdd
ladislav-moravec <moravec.ld@gmail.com>
2024-05-18 15:49:05 +0200

    chat javascript python backend

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PRIVATE_KEY = "c7139fb3-eb22-4b0d-8fea-ebb7ea85b5e3"
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Project-ID": PRIVATE_KEY }
+    )
+    return response.json()
+
+
+
